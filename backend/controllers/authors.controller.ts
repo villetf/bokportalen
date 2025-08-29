05669b0b4bd3973f86e9178a4e57ca7c44d12890
Vilhelm Fontell <ville@fontell.se>
2025-08-29 14:09:25 +0200

    lade till funktionalitet för att hämta alla författare

--- backend/controllers/authors.controller.ts
+++ backend/controllers/authors.controller.ts
@@ -2,13 +2,20 @@ import type { Request, Response } from 'express';
 import { AuthorsService } from '../services/authors.services.js';
 
 export class AuthorsController {
-   static async getAllAuthors(req: Request, res: Response) {
-      const { firstName, lastName } = req.query;
-      const author = await AuthorsService.getAuthorByName(firstName as string, lastName as string);
-      if (!author) {
-         res.status(404).json({ error: 'Author not found' });
+   static async getAuthors(req: Request, res: Response) {
+      if (req.query === undefined || Object.keys(req.query).length === 0) {
+         const allAuthors = await AuthorsService.getAllAuthors();
+         res.json(allAuthors);
+         return;
       }
 
-      res.json(author);
+      const { firstName, lastName } = req.query;
+      const authors = await AuthorsService.getAuthorsByQuery(firstName as string, lastName as string);
+      if (authors.length === 0) {
+         res.status(404).json({ error: 'Author not found' });
+         return;
+      }
+
+      res.json(authors);
    }
 }
