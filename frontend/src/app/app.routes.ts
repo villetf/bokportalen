c927852f628bb6f1a505f9ab48d01f65b9c1080e
Vilhelm Fontell <ville@fontell.se>
2025-09-02 17:11:58 +0200

    Implementera router (issue #47)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './features/home/pages/home-page/home-page';
+import { AllBooks } from './features/books/pages/all-books/all-books';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', component: HomePage },
+   { path: 'books', component: AllBooks }
+];
---
+++ frontend/src/app/features/books/pages/all-books/all-books.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-books',
+  imports: [],
+  templateUrl: './all-books.html',
+  styles: ``
+})
+export class AllBooks {
+
+}
