93f36be91cad5db00f7f33b3ec9fc8810f69ae6a
Vilhelm Fontell <ville@fontell.se>
2025-09-05 11:27:33 +0200

    lade till ny route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './features/home/pages/home-page/home-page';
 import { AllBooks } from './features/books/pages/all-books/all-books';
+import { BookPage } from './features/books/pages/book-page/book-page';
 
 export const routes: Routes = [
    { path: '', component: HomePage },
-   { path: 'books', component: AllBooks }
+   { path: 'books', component: AllBooks },
+   { path: 'books/:id', component: BookPage },
 ];
