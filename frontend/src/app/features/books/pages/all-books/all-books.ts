77753f228dedc36a44df5af2f70da587376bab3c
Vilhelm Fontell <ville@fontell.se>
2025-09-05 11:28:30 +0200

    bytte fetch till hämtning med observable

--- frontend/src/app/features/books/pages/all-books/all-books.ts
+++ frontend/src/app/features/books/pages/all-books/all-books.ts
@@ -1,25 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { Book } from '../../../../types/Book.model';
 import { BookCard } from "../../components/book-card/book-card";
+import { BooksService } from '../../../../services/booksService';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-all-books',
-  imports: [BookCard],
+  imports: [BookCard, AsyncPipe],
   templateUrl: './all-books.html',
   styles: ``
 })
 export class AllBooks {
-   books = signal(<Book[]>[]);
+   books$!: Observable<Book[]>;
+
+   constructor(private booksService: BooksService) {}
 
    ngOnInit(): void {
-      fetch('http://localhost:3000/books')
-         .then(response => response.json())
-         .then (data => data.map((book: Book) => {
-            if (!book.coverLink) {
-               book.coverLink = 'book-cover-placeholder.png';
-            }
-            return book;
-         }))
-         .then(data => this.books.set(data));
+      this.books$ = this.booksService.getBooks();
    }
 }
