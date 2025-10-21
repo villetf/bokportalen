8e87075ac197a15e5a7387e4062d2883103c5a00
Vilhelm Fontell <ville@fontell.se>
2025-10-21 11:35:15 +0200

    Skapa funktion för att lägga till språk (issue #77)

--- frontend/src/app/features/languages/components/add-language/add-language.ts
+++ frontend/src/app/features/languages/components/add-language/add-language.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Button } from '../../../../shared/components/button/button';
+import { HotToastService } from '@ngxpert/hot-toast';
+import { HttpErrorResponse } from '@angular/common/http';
+import { LanguagesService } from '../../../../services/languagesService';
+import { Language } from '../../../../types/Language.model';
 
 @Component({
    selector: 'app-add-language',
-   imports: [],
+   imports: [ReactiveFormsModule, Button],
    templateUrl: './add-language.html',
    styles: ''
 })
 export class AddLanguage {
+   form!: FormGroup;
 
+   constructor(
+      private fb: FormBuilder,
+      private toast: HotToastService,
+      private languagesService: LanguagesService
+   ) {}
+
+   ngOnInit(): void {
+      this.form = this.fb.group({
+         name: [[], [Validators.required]]
+      });
+   }
+
+   resetForm() {
+      this.form.reset({
+         name: []
+      });
+   }
+
+   save() {
+      if (this.form.valid) {
+         const newLanguage: Language = this.form.value;
+
+         this.languagesService.addLanguage(newLanguage)
+            .pipe(
+               this.toast.observe({
+                  loading: 'Lägger till språk...',
+                  success: (res) => {
+                     this.resetForm();
+                     return `Lade till språket ${(res as Language).name}!`;
+                  },
+                  error: (err) => {
+                     if ((err as HttpErrorResponse).status == 409) {
+                        return 'Detta språk finns redan.';
+                     }
+
+                     return 'Något gick fel vid skapande av språk.';
+                  }
+               })
+            )
+            .subscribe();
+      } else {
+         this.toast.error('Uppgifterna är inte giltiga.');
+      }
+   }
 }
--- frontend/src/app/services/languagesService.ts
+++ frontend/src/app/services/languagesService.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Language } from '../types/Language.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 
 @Injectable({ providedIn: 'root' })
 export class LanguagesService {
+   constructor(private http: HttpClient) {}
+
    async getAllLanguages(): Promise<Language[]> {
       const response = await fetch('http://localhost:3000/languages');
       const data: Language[] = await response.json();
       return data.sort((a, b) => a.name.localeCompare(b.name));
+   }
+
+   addLanguage(language: Language) {
+      return this.http.post('http://localhost:3000/languages/', language).pipe(
+         catchError(err => {
+            console.error('Error when posting language:', err);
+            return throwError(() => err);
+         })
+      );
    }
 }
