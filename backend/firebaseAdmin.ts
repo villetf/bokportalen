ff968a186739ef70576b0c7084dc520ffe662afd
Vilhelm Fontell <ville@fontell.se>
2026-07-30 17:40:47 +0200

    bröt ut firebase-auth till separat json-fil

--- backend/firebaseAdmin.ts
+++ backend/firebaseAdmin.ts
@@ -1,9 +1,10 @@
 import admin, { ServiceAccount } from 'firebase-admin';
 import dotenv from 'dotenv';
-
+import fs from 'fs';
 dotenv.config();
+
 const serviceAccount = JSON.parse(
-   process.env.FIREBASE_SERVICE_ACCOUNT as string
+   fs.readFileSync(process.env.FIREBASE_SERVICE_ACCOUNT_PATH!, 'utf8')
 );
 
 admin.initializeApp({
